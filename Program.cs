f9b43c9c925d547896a0c6a3b5afd7fcfe472796
Jeffrey Kranenburg <jeff_mbp@Jeffreys-MacBook-Pro.local>
2018-04-23 16:52:42 +1200

    Update Gist

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace refvaltype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Classes = reference types (Arrays and lists<T> are both classes)
+                Structs = value types
+            */
+
+            Console.WriteLine("DATE TIME = STRUCT");
+
+            //DateTime is a struct (value type, creates a copy of...)
+            DateTime date = new DateTime(2010, 1, 1);
+            DateTime datecopy = date; //copy the value of date into a new variable 
+
+            date = date.AddYears(1); //change the first variable
+
+            Console.WriteLine(date.Year); // returns 2011
+            Console.WriteLine(datecopy.Year); // returns 2010
+
+
+            //Array is a class (reference type, references to the original variables)
+            Console.WriteLine("ARRAY = CLASS");
+
+            int[] number = new int[] {1, 2, 3, 4, 5};
+            int[] numberCopy = number;
+
+            number[0] = 10;
+
+            Console.WriteLine(string.Join(",", number));
+            Console.WriteLine(string.Join(",", numberCopy));
+
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Reference and Value Types
+
+Just a quick reference guide to show the difference between value and reference types.
+
+* **Classes** = reference types
+* **Structs** = value types
+
+Reference Types change the original value when the variable is placed in a new variable
+
+Value Types keeps the original value value when the variable is placed in a new variable
